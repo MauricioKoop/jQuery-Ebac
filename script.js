e4454b1cf7526fb36edbd1ea29c244563e4a8812
Mauricio Eduardo <mauricioeduardokoop@gmail.com>
2024-10-20 18:12:00 -0300

    Estiliza a página, e aplica lógica que contabilida e exibe a quantidade de cada item escolhido.

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let products = [
-    {id: 1, name: 'Curso 1', price: 10.99, image: 'img/product1.jpg'},
-    {id: 2, name: 'Curso 2', price: 20.99, image: 'img/product2.jpg'},
-    {id: 3, name: 'Curso 3', price: 30.99, image: 'img/product3.jpg'},
-    {id: 4, name: 'Curso 4', price: 40.99, image: 'img/product4.jpg'},
-    {id: 5, name: 'Curso 5', price: 50.99, image: 'img/product5.jpg'},
-    {id: 6, name: 'Curso 6', price: 60.99, image: 'img/product6.jpg'},
-    {id: 7, name: 'Curso 7', price: 70.99, image: 'img/product7.jpg'},
-    {id: 8, name: 'Curso 8', price: 80.99, image: 'img/product8.jpg'},
-    {id: 9, name: 'Curso 9', price: 90.99, image: 'img/product9.jpg'}
+    {id: 1, name: 'Curso 1', price: 10.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 2, name: 'Curso 2', price: 20.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 3, name: 'Curso 3', price: 30.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 4, name: 'Curso 4', price: 40.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 5, name: 'Curso 5', price: 50.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 6, name: 'Curso 6', price: 60.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 7, name: 'Curso 7', price: 70.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 8, name: 'Curso 8', price: 80.99, amount: 1, image: 'img/product1.jpg'},
+    {id: 9, name: 'Curso 9', price: 90.99, amount: 1, image: 'img/product1.jpg'}
 ];
 
 let cart = [];
@@ -26,25 +26,40 @@ function renderProducts(){
         `;
         productGrid.appendChild(productDiv);
         productDiv.querySelector('button').addEventListener('click', () => {
-            addTocart(product.id);
+            addTocart(product);
         })
     })
 }
 
-function addTocart(productId) {
-    let product = products.find((product) => product.id === productId);
-    cart.push(product);
+function addTocart(currentProduct) {
+    let product = products.find((product) => product.id === currentProduct.id);
+
+    if (cart.length === 0) {
+        cart.push(product);
+    }else{
+        let equalProduct = cart.find((product) => product.id === currentProduct.id);
+        if(equalProduct != undefined){
+            equalProduct.amount += 1;
+            cart.push(equalProduct);
+        }else{
+            cart.push(product);
+        }
+    }
+
     renderCart();
 }
 
 function renderCart() {
     let cartTable = document.querySelector('.cart table tbody');
     cartTable.innerHTML = '';
-    cart.forEach((product) => {
+
+    let cartFiltered = cart.filter((current, i) => cart.indexOf(current) === i );
+
+    cartFiltered.forEach((product) => {
         let cartRow = document.createElement('tr');
         cartRow.innerHTML = `
             <td>${product.name}</td>
-            <td>1</td>
+            <td>${product.amount}</td>
             <td>${product.price}</td>
             <td>${product.price}</td>
         `;
@@ -57,10 +72,6 @@ function updateTotal(){
     let total = cart.reduce((acc, product) => acc + product.price, 0);
     document.getElementById('total').textContent = `$${total.toFixed(2)}`;
 }
-
-// let currentProduct = 0;
-// let prev = document.querySelector('.prev');
-// let next = document.querySelector('next');
 
 document.getElementById('checkout').addEventListener('click', () => {
     if(cart.length === 0)
